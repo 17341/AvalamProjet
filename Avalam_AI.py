b24fa5c0a1f8173f506756209903fc1c80fddc3d
thierry <thierrymoumi@gmail.com>
2020-05-03 19:50:35 +0200

    comment

--- Avalam_AI.py
+++ Avalam_AI.py
@@ -21,22 +21,32 @@ class Server:
         print(self.f, self.t)
         
         return {"move": {"from": self.f ,"to" : self.t},"message": self.msg }
-
+    
+    ##############################################################################################
+    ##### On prend la situation de la partie dans une nouvelle liste(position) qui      ##########
+    ##### a comme valeur de liste une liste avec les lignes et colonnes des différentes ##########
+    ##### positions.                                                                    ##########
+    ##############################################################################################
     def pion_position(self):
         self.position = []
         self.coup_possible = {}
-        self.liste = self.body["game"]       # On prend la situation de la partie
-        for l in range(9):              # dans une nouvelle liste (position) qui a comme valeur de liste une 
-            for c in range(9):          # liste avec les lignes et les colonnes des differentes posisions.
+        self.liste = self.body["game"]       
+        for l in range(9):              
+            for c in range(9):          
                 if len(self.liste[l][c]) < 5 and len(self.liste[l][c]) != 0:
                     self.position.append([l,c])
                     self.coup_possible[l,c] = []
         return(self.position)
 
-    def can_move(self):                 # On scann autour des positions des pions pour voir les coups possibles que
-        for f in self.position:      # peut faire le pion , on sauvegarde cela dans un dict avec comme clé la 
-            l = f[0]                 # position des pions et comme valeur une liste des different positions possible que
-            c = f[1]                 # peut faire ce dernier.
+    #####################################################################################################
+    #####On scanne autour des positions des pions pour voir les coups possibles que peut faire ##########
+    #####le pion, on sauvegarde cela dans un dictionnaire avec comme clé la position des pions ##########
+    #####et comme valeur une liste des différentes positions possibles que peut faire ce derniers########                                                                 
+    #####################################################################################################
+    def can_move(self):                
+        for f in self.position:      
+            l = f[0]                 
+            c = f[1]           
             if c < 8 and len(self.liste[l][c+1]) < 5 and len(self.liste[l][c+1]) != 0 : 
                 if (len(self.liste[l][c]) + len(self.liste[l][c+1])) <= 5 : 
                     self.coup_possible[l,c].append([l,c+1])
@@ -63,12 +73,18 @@ class Server:
                     self.coup_possible[l,c].append([l+1,c+1])             
         return(self.coup_possible)
     
+    #####################################################################################################
+    #####                   Identification en récupérant les données du jeu                     #########
+    #####################################################################################################
     def joueur(self):
         if self.body["you"] == self.body["players"][0]:
             self.moi = 0
         else : 
             self.moi = 1
-            
+    
+    #######################################################################################################################
+    ##### Information sur le jeu, récupération des données sur les positions, les coups possibles des pions et le joueur###             
+    #######################################################################################################################      
     def info_jeu(self):
         self.pion_position()
         self.can_move()
@@ -104,7 +120,11 @@ class Server:
                         if len(self.body["game"][f[0]][f[1]]) + len(self.body["game"][pos[0]][pos[1]]) != 5 :
                             self.minpoint[f].append(pos)
         return(self.minpoint)
-
+#####################################################################################################################
+#####On récupere les données des fonctions précedentes dans notre IA,on parcoure le dictionnaire en récupérant ######
+##### les clé(dans f) puis on vérifie l'une des conditions tout en éffectuant un break à la fin de la condition,#####
+##### dans le cas contraire on fait jouer le random                                                             #####
+#####################################################################################################################
     def IA(self):
         self.info_jeu()
         self.tac_isolate()
@@ -122,7 +142,7 @@ class Server:
                 self.t = choice(self.five[f])
                 print(self.f, self.t)
                 break
-            #AVEC LES PIONS ADVERSAIRE :    
+            #AVEC LES PIONS DE L'ADVERSAIRE :    
             if len(self.minpoint[f]) > 0:
                 self.f = choice(self.coup_possible[f])
                 self.t = list(f)
